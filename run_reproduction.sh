e8f02b4d6500ec9f998b6d0a440037b9c5ad34b1
Stefan Penner <stef@iamstef.net>
2026-01-08 18:43:38 +0000

    fixup repro

--- run_reproduction.sh
+++ run_reproduction.sh
@@ -12,7 +12,7 @@ rm -f /tmp/bazel_minimal_repro_marker
 bazel coverage //:minimal_test \
   --cache_test_results=no \
   --flaky_test_attempts=2 \
+  --strategy=CoverageReport=local \
+  --test_output=all \
   --experimental_split_coverage_postprocessing \
   --experimental_fetch_all_coverage_outputs \
-  --strategy=CoverageReport=local \
-  --test_output=all
