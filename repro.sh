d752ea84a79b079e83623736f354d8d8a13fbdb1
Stefan Penner <stef@iamstef.net>
2026-01-12 12:29:20 -0700

    this seems to not effect the failure, but we use it, so let's keep it here

--- repro.sh
+++ repro.sh
@@ -4,10 +4,12 @@ set -e
 mkdir -p /tmp/bazel-repro
 rm -f /tmp/bazel-repro/marker
 
+bazel clean
 bazel coverage //:minimal_test \
   --cache_test_results=no \
   --flaky_test_attempts=2 \
   --test_output=all \
+  --strategy=CoverageReport=local \
   --experimental_split_coverage_postprocessing \
   --experimental_fetch_all_coverage_outputs \
   --sandbox_add_mount_pair=/tmp/bazel-repro
