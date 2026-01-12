03b4f96de4afd027079998069e4384624b3b0c73
Stefan Penner <stef@iamstef.net>
2026-01-12 11:44:07 -0700

    repro on linux \w good sandboxing

--- repro.sh
+++ repro.sh
@@ -12,6 +12,7 @@ bazel coverage //:minimal_test \
   --cache_test_results=no \
   --flaky_test_attempts=2 \
   --strategy=CoverageReport=local \
-  --test_output=errors \
+  --test_output=all \
   --experimental_split_coverage_postprocessing \
-  --experimental_fetch_all_coverage_outputs
+  --experimental_fetch_all_coverage_outputs \
+  --sandbox_add_mount_pair=/tmp
